eaa34c95e60442047f3a988a2efc578f22226da4
Vitalii Elenhaupt <velenhaupt@gmail.com>
2018-08-25 14:53:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## brutality.el
+
+:construction:
+
+Make Emacs play nicely with iTerm2 and tmux.
+
+This is a port of [Vitality.vim](https://github.com/sjl/vitality.vim) plugin to Emacs
---
+++ brutality.el
@@ -0,0 +1,40 @@
+;;; brutality.el --- Make Emacs play nicely with iTerm2 and tmux.
+
+;; Copyright © 2018 Vitalii Elenhaupt <velenhaupt@gmail.com>
+;; Author: Vitalii Elenhaupt
+;; URL: https://github.com/veelenga/brutality.el
+;; Keywords: convenience
+;; Package-Requires: ((emacs "24.4"))
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Usage:
+
+;;; Code:
+
+(defgroup ameba nil
+  "Make Emacs play nicely with iTerm2 and tmux"
+  :prefix "brutality-"
+  :group 'convenience
+  :group 'tools
+  :link '(url-link :tag "GitHub" "https://github.com/veelenga/brutality.el"))
+
+(provide 'brutality)
+;;; brutality.el ends here
